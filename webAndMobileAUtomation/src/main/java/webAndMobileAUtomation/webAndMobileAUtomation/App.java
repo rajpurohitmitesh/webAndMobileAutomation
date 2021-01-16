204e0cd23e316a78da173ffe7e1fde478933d48d
rajpurohitmitesh <mitesh.rajpurohit0191@gmail.com>
2021-01-16 14:02:32 +0530

    created maven framework skelton

---
+++ webAndMobileAUtomation/src/main/java/webAndMobileAUtomation/webAndMobileAUtomation/App.java
@@ -0,0 +1,13 @@
+package webAndMobileAUtomation.webAndMobileAUtomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
