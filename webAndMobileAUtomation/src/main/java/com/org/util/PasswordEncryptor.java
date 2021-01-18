c4d3174657d801f2f1c286ea34b702431a55425a
rajpurohitmitesh <mitesh.rajpurohit0191@gmail.com>
2021-01-18 22:12:03 +0530

    added utility class to encrypt and decrypt password

--- webAndMobileAUtomation/src/main/java/com/org/util/PasswordEncryptor.java
+++ webAndMobileAUtomation/src/main/java/com/org/util/PasswordEncryptor.java
@@ -1,5 +1,33 @@
 package com.org.util;
 
-public class PasswordEncryptor {
+import java.security.CryptoPrimitive;
+import java.util.Base64;
+import java.util.Scanner;
 
+public class PasswordEncryptor {
+	Scanner sc = new Scanner(System.in);
+	String plain;
+
+	public void getEncrypt() {
+		System.out.println("Enter password to get encrypted :");
+		plain = sc.nextLine();
+		byte[] encodedBytes = Base64.getEncoder().encode(plain.getBytes());
+		System.out.println("Encrypted password :" + new String(encodedBytes));
+	}
+	
+	public void getDecrypt() {
+		System.out.println("Enter encrypted password to get decrypted :");
+		plain = sc.nextLine();
+		byte[] encodedBytes = Base64.getDecoder().decode(plain.getBytes());
+		System.out.println("Decrypted password :" + new String(encodedBytes));
+	}
+	
+	public static void main(String args[]) {
+		PasswordEncryptor obj = new PasswordEncryptor();
+	//	obj.getEncrypt();
+	//	obj.getDecrypt();			
+	}
+	
+	
+	
 }
